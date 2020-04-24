4bf31104547ada0ba861dc5d49d6409883f21d3e
David Svensson <davidsvson@gmail.com>
2020-04-24 11:05:16 +0200

    Saving to database

--- app/src/main/java/com/example/map19roomintro/MainActivity.kt
+++ app/src/main/java/com/example/map19roomintro/MainActivity.kt
@@ -2,16 +2,32 @@ package com.example.map19roomintro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Database
+import androidx.room.Room
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var db : AppDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "shopping-items")
+            .build()
+
+
         val item = Item(0, "Mjölk", false, "kyl" )
 
 
-
+       // saveItem(item)
     }
+
+    fun saveItem(item: Item) {
+        GlobalScope.async(Dispatchers.IO) {   db.itemDao().insert(item) }
+    }
+
 }
