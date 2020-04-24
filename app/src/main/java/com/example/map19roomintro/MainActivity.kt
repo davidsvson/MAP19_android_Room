1859b25a191c4b498cec17f9d097e1eca6bf9562
David Svensson <davidsvson@gmail.com>
2020-04-24 13:27:34 +0200

    Coroutinescope

--- app/src/main/java/com/example/map19roomintro/MainActivity.kt
+++ app/src/main/java/com/example/map19roomintro/MainActivity.kt
@@ -5,18 +5,24 @@ import android.os.Bundle
 import androidx.room.Database
 import androidx.room.Room
 import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , CoroutineScope {
+
+    private lateinit var job : Job
+    override val coroutineContext : CoroutineContext
+        get() = Dispatchers.Main + job
 
     private lateinit var db : AppDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        job = Job()
 
         db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "shopping-items")
+            .fallbackToDestructiveMigration()
             .build()
-
 
         val item = Item(0, "Smör", false, "kyl" )
         val item2 = Item(0, "ost", false, "kyl" )
@@ -29,7 +35,10 @@ class MainActivity : AppCompatActivity() {
         val items = loadAllItems()
         val itemsByCat = loadByCategory("grönsak")
 
-        GlobalScope.launch {
+        // nu används vår activity som scope eftersom den implemneterar CoroutineScope
+        // i stället kan vi använda GlobalScope i så fall skriver GlobalScope.launch i stället
+        // men rekomendationen är att vara restrektiv med att använda globalscope
+       launch {
             itemsByCat.await().forEach {
                 println("!!! ${it.name}")
             }
@@ -37,22 +46,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun saveItem(item: Item) {
-        GlobalScope.async(Dispatchers.IO) {   db.itemDao().insert(item) }
+        async(Dispatchers.IO) {   db.itemDao().insert(item) }
     }
 
     fun loadAllItems() : Deferred<List<Item>>  =
-         GlobalScope.async(Dispatchers.IO) {
+         async(Dispatchers.IO) {
             db.itemDao().getAll()
         }
 
     fun loadByCategory(category: String) :  Deferred<List<Item>>  =
-        GlobalScope.async(Dispatchers.IO) {
+        async(Dispatchers.IO) {
             db.itemDao().findByCategory(category)
         }
 
+    override fun onDestroy() {
+        job.cancel()
+        super.onDestroy()
 
-
-
-
+    }
 
 }
